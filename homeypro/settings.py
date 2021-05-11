4e05703f8454b0f5454b11f5f5df7171c524c7c7
Hayden Harris <haydenharris0@gmail.com>
2021-05-11 11:39:02 -0500

    updated heroku variables

--- homeypro/settings.py
+++ homeypro/settings.py
@@ -26,7 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+if config('ENVIRONMENT') == "production":
+    DEBUG = False
+    print("*!!!!!!*!!*", config('ENVIRONMENT'))
+if config('ENVIRONMENT') == "development":
+    DEBUG = True
+    print("*!!!!!!*!!* DEBUG=", DEBUG,  config('ENVIRONMENT'))
+
 
 ALLOWED_HOSTS = ['herokuapp.com', '.localhost', '127.0.0.1', '[::1]']
 
